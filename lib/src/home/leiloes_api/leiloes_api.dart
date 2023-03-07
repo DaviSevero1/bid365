d343a21cd470628af744578696e5590dc295d51f
Davi Severo <severosantana2014@gmail.com>
2023-03-07 16:34:55 -0300

    Implementando getAll 1/3

--- lib/src/home/leiloes_api/leiloes_api.dart
+++ lib/src/home/leiloes_api/leiloes_api.dart
@@ -1,5 +1,24 @@
-// class LeiloesApi{
-//   static List<Leilao> getLeilao(){
+import 'package:http/http.dart' as http;
 
-//   }
-// }
+class Leilao_api {
+  Future<bool> getAll() async {
+    var url = Uri.parse('https://bis365.com.br/bid365/api/v1/auctions');
+    var header = {
+      "app": "ee85db5d-ebec-4548-a62b-ae8c68955d31",
+      "Content-Type": "application/json"
+    };
+
+    var response = await http.get(url, headers: header);
+
+    print('Response status: ${response.statusCode}');
+    print('Response body: ${response.body}');
+    if (response.statusCode == 200) {
+      return true;
+    } else {
+      print('Erro ao buscar leilões');
+    }
+    return false;
+  }
+
+  // final data =  login();
+}
